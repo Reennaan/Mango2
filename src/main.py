cb270c51a14930e8af5ab0c37999e8a8884a6671
Renan <qwerty142999@gmail.com>
2026-02-28 17:03:39 -0300

    fixing-callback-errors

--- src/main.py
+++ src/main.py
@@ -5,52 +5,65 @@ import pprint
 import json
 from pathlib import Path
 
+baseurl = "https://www.anime-planet.com/"
 
 
 class Api:
+    def __init__(self):
+        self.pending_download = None
 
     def fetch(self):
-        baseurl = "https://www.anime-planet.com/"
         url = "https://www.anime-planet.com/manga/read-online/"
 
         scraper = cloudscraper.create_scraper()
         response = scraper.get(url)
 
-        #print("Status:", response.status_code)
-        #print(response.text[:500])
         soup = BeautifulSoup(response.text, 'html.parser')
         grid = soup.find("ul", class_="cardDeck")
-        #pprint.pprint(grid)
 
-
-        #names = grid.select("h3", class_="cardName")
         gimgs = grid.select("ul > li.card > a.tooltip > div.crop > img")
         gref = grid.select("ul > li.card > a.tooltip")
 
-        for i in range(0,10):
-            #print(names[i].text)
+        for i in range(0, 10):
             imgUrl = gimgs[i]["data-src"]
-            fullHref = baseurl + gref[i]["href"].lstrip('/') 
+            fullHref = baseurl + gref[i]["href"].lstrip('/')
             title = gimgs[i]["alt"]
-            #print(imgUrl)
-            #self.buildMangaInfo(title,imgUrl,fullHref)
-            window.evaluate_js(f"""window.buildMangaInfo({json.dumps(title)},{json.dumps(imgUrl)},{json.dumps(fullHref)})""")
+            jsCall = f"window.buildMangaInfo({json.dumps(title)},{json.dumps(imgUrl)},{json.dumps(fullHref)})"
+            window.evaluate_js(jsCall)
 
-            
-    def mangaDownloadPage(self,imgUrl,url):
+    def getChapters(self, imgUrl, url, mangaName):
         scraper = cloudscraper.create_scraper()
         response = scraper.get(url)
 
         soup = BeautifulSoup(response.text, 'html.parser')
+        grid = soup.find("ul", class_="cardDeck")
         chapter = soup.find_all("h3", class_="cardName")
-        pprint.pprint(chapter)
+        chapterNames = [item.get_text(strip=True) for item in chapter]
 
+        hrefs = grid.select("ul > li.card > a")
+        downloadLinks = [item.get("href") for item in hrefs]
 
+        #guarda os dados temporariamente os dados para que a próxima página "download.html" obtenha eles com segurança
+        self.pending_download = {
+            "chapters": chapterNames,
+            "img": imgUrl,
+            "title": mangaName,
+            "downloadLinks": downloadLinks,
+        }
+
+        return download_file.as_uri()#agora a mudança de pagina é feita através do js
+
+    def getPendingDownloadData(self):
+        return self.pending_download
+
+    def downloadFile(self,link):
+        print(link)
 
 
 api = Api()
 base = Path(__file__).resolve().parent.parent
 index_file = base / "assets" / "index.html"
+download_file = base / "assets" / "download.html"
 window = webview.create_window("Mango", index_file.as_uri(), js_api=api)
 webview.start()
 
